83aea94058e3d7aa8f989096983e8eeed401c894
LEE.CHANG.HUN <dolshei@gmail.com>
2022-09-17 21:00:32 +0900

    영화별 평균 점수/리뷰 페이지 처리

--- src/main/java/com/example/moviereview/repository/MovieRepository.java
+++ src/main/java/com/example/moviereview/repository/MovieRepository.java
@@ -1,8 +1,19 @@
 package com.example.moviereview.repository;
 
 import com.example.moviereview.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    // Entity 를 기준으로 Query를 작성한다.
+    @Query("SELECT m, mi, avg(coalesce(r.grade, 0)), COUNT(DISTINCT r) " +
+            "FROM Movie m LEFT OUTER JOIN MovieImage mi " +
+            "ON m = mi.movie " +
+            "LEFT OUTER JOIN Review r " +
+            "ON m = r.movie GROUP BY m ")
+    Page<Object[]> getListPage(Pageable pageable);
 
 }
