9a993229af42001b3d53ebb0f60cc1103fbaea84
LEE.CHANG.HUN <dolshei@gmail.com>
2022-09-18 19:40:04 +0900

    특정 영화의 이미지와 평균점수, 리뷰 갯수

--- src/main/java/com/example/moviereview/repository/MovieRepository.java
+++ src/main/java/com/example/moviereview/repository/MovieRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
-    // Entity 를 기준으로 Query를 작성한다.
+    // Entity 를 기준으로 Query 를 작성한다.
     @Query("SELECT m, mi, avg(coalesce(r.grade, 0)), COUNT(DISTINCT r) " +
             "FROM Movie m LEFT OUTER JOIN MovieImage mi " +
             "ON m = mi.movie " +
@@ -16,4 +19,11 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
             "ON m = r.movie GROUP BY m ")
     Page<Object[]> getListPage(Pageable pageable);
 
+    @Query("SELECT m, mi " +
+            "FROM Movie m LEFT OUTER JOIN MovieImage mi " +
+            "ON m = mi.movie " +
+            "   LEFT OUTER JOIN Review r " +
+            "   ON m = r.movie " +
+            "WHERE m.mno = :mno GROUP BY mi ")
+    List<Object[]> getMovieWithAll(@Param("mno") Long mno);     // 특정 영화 조회
 }
--- src/test/java/com/example/moviereview/repository/MovieRepositoryTests.java
+++ src/test/java/com/example/moviereview/repository/MovieRepositoryTests.java
@@ -12,6 +12,7 @@ import org.springframework.test.annotation.Commit;
 
 import javax.transaction.Transactional;
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -55,6 +56,18 @@ public class MovieRepositoryTests {
 
         for (Object[] objects : result.getContent()) {
             System.out.println(Arrays.toString(objects));
+        }
+    }
+
+    @Test
+    public void testGetMovieWithAll() {
+
+        List<Object[]> result = movieRepository.getMovieWithAll(93L);
+
+        System.out.println(result);
+
+        for (Object[] arr : result) {
+            System.out.println(Arrays.toString(arr));
         }
     }
 }
